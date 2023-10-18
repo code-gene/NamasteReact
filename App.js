8a80cb07d5f96a37f836a8f5abe639775548086c
code-gene <abhishek.career10@gmail.com>
2023-10-18 05:56:08 +0530

    First commit

---
+++ App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+const parent = React.createElement(
+  "div",
+  { id: "parent" },
+  React.createElement(
+    "div",
+    { id: "child" },
+    React.createElement("h1", {}, "I am an h1 tag")
+  )
+);
+
+// const heading = React.createElement("h1", {}, "Hello JS!");
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(parent);
